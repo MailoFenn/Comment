c3095f2b1cdf5a884eea198c5e426b24b6d1833a
Mailo_Fenn <nikitakulkov7@gmail.com>
2020-04-12 00:19:03 +0300

    seperation comments to form and comments

--- src/index.js
+++ src/index.js
@@ -1,113 +1,130 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Comment extends React.Component {
-    constructor() {
-        super();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
         this.state = {
             comments: [],
             newName: '',
             newText: '',                   
-        }      
+        }
+        
+        this.textAreaStyle = {};
     }
 
     componentDidMount() {
-        if(localStorage.getItem('state') !== null) {
-            let comments = localStorage.getItem('state');
-            comments = JSON.parse(comments);
-            let arr = [];
-        
-            comments.map((item) => {
-                arr.push(item);
-            });
+        if(localStorage.getItem('state') != null) {
+            let comments = JSON.parse(localStorage.getItem('state'));
 
-            this.setState({comments: arr})
-
+            this.setState({comments})
         }
     }
 
     addComment() {
-        let comments = this.state.comments;
+        let comments = [...this.state.comments];
         let date = new Date;
         let localTime = date.toLocaleTimeString();
         let localDate = date.toLocaleDateString();
 
         if(this.state.newText === '') {
-            let textArea = document.querySelector('textarea');
-            textArea.style.borderColor = 'red';
+            this.textAreaStyle = {borderColor: 'red'};
         } else if(this.state.newName === '') {
-            comments.push({text: this.state.newText, author: 'Аноним', date: localDate, time: localTime});
-            this.setState({comments, newName: '', newText: ''});
+            comments.push({text: this.state.newText, author: 'Аноним', date: localDate, time: localTime});   
         } else {
             comments.push({text: this.state.newText, author: this.state.newName, date: localDate, time: localTime});
-            this.setState({comments, newName: '', newText: ''});
         }
         
-
-        localStorage.setItem('state', JSON.stringify(this.state.comments));
+        this.setState({comments, newName: '', newText: ''});
+        
+        localStorage.setItem('state', JSON.stringify(comments));
     }
 
     delete(key) {
-        let comments = this.state.comments;
+        let comments = [...this.state.comments];
         comments.splice(key, 1);
 
         console.log(comments);
 
         this.setState({comments});
 
-        localStorage.setItem('state', JSON.stringify(this.state.comments));
+        localStorage.setItem('state', JSON.stringify(comments));
     }
 
     render() {
         return(
-        <div className='comments'>
-            <p>Имя: <input className='inputname'
-            value={this.state.newName}
-            onChange={(ev) => {
-                this.setState({newName: ev.target.value});
-            }}
-            type='text'
-            name='author'></input></p>
-
-            <p><textarea value={this.state.newText}
-            onClick={(ev) => {
-                ev.target.style = 'border-color: black';
-            }}
-            onChange={(ev) => {
-                this.setState({newText: ev.target.value});
-            }}></textarea></p>
-            
-            <p><button onClick={() => {
-                this.addComment();
-            }}>Отправить</button></p>
-
-            {this.state.comments.map((comment, i) => {
-            return(
-            <div key={i} className='comment'>
-                <p className='name'>{comment.author}</p>
-                <p className='time'>{comment.time}</p>
-                <p className='time'>{comment.date}</p>
-                <img className='delete'
-                onClick={() => {
-                    this.delete(i);
+            <div className='comments'>
+                <Form
+                textAreaStyle={this.textAreaStyle}
+                valueName={this.state.newName}
+                valueText={this.state.newText}
+                onChangeName={(ev) => {
+                    this.setState({newName: ev.target.value});
                 }}
-                src='src/delete.png'></img>
-                <p className='text'>{comment.text}</p> 
-            </div> 
-            );
-            })}
-
-            {
-        }
-           
-        </div>  
-        )
-
+                onChangeText={(ev) => {
+                    this.setState({newText: ev.target.value});
+                }}
+                onClickButtton={() => {
+                    this.addComment();
+                }} />
+                {this.state.comments.map((comments, i) => {
+                    return(
+                        <Comments
+                        key={i} 
+                        author={comments.author}
+                        time={comments.time}
+                        date={comments.date}
+                        text={comments.text}
+                        onClick={() => {
+                            this.delete(i);
+                        }} />
+                    );
+                })}
+                
+            </div>
+        );
     }
 }
 
+function Form(props) {
+    return(
+        <div>
+            <p>Имя: <input
+            className='inputname'
+            value={props.valueName}
+            onChange={props.onChangeName}
+            type='text'
+            name='author'></input></p>
+
+            <p><textarea
+            style={props.textAreaStyle}
+            value={props.valueText}
+            onClick={(ev) => {
+                ev.target.style = 'border-color: black';
+            }}
+            onChange={props.onChangeText}></textarea></p>
+            
+            <p><button onClick={props.onClickButtton}>Отправить</button></p>
+        </div>
+    );
+}
+
+function Comments(props) {
+    return(
+        <div key={props.key} className='comment'>
+            <p className='name'>{props.author}</p>
+            <p className='time'>{props.time}</p>
+            <p className='time'>{props.date}</p>
+            <img className='delete'
+            onClick={props.onClick}
+            src='src/delete.png'></img>
+            <p className='text'>{props.text}</p> 
+        </div> 
+    );
+}
+
 ReactDOM.render(
-    <Comment/>,
+    <App/>,
     document.querySelector('#app')
 );
